70dbc69bf2ef1612f339baa95580b5c72314d507
Nathan Hagen <nathan@aggronn.com>
2015-01-12 17:29:53 -0500

    Implemented react-router

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -3,6 +3,10 @@
 var React = require('react');
 
 var NavBar = require('./navBar.jsx');
+var Router = require('react-router');
+
+var Route = require('react-router').Route;
+var RouteHandler = require('react-router').RouteHandler;
 
 window.React = React; 
 
@@ -10,10 +14,39 @@ var App = React.createClass({
   render: function() {
     return <div className="container">
         <NavBar />
+        <RouteHandler />
 
       </div>
   }
 });
 
-React.render(<App name="lol!"/>, document.body);
+var Inventory = React.createClass({
+  render: function() {
+    return <h1>Inventory Page</h1>
+  }
+});
+
+var Campaigns = React.createClass({
+  render: function() {
+    return <h1>Campaigns Page</h1>
+  }
+});
+
+var Reporting = React.createClass({
+  render: function() {
+    return <h1>Reporting Page</h1>
+  }
+});
+
+var routes = (
+  <Route handler={App}>
+    <Route name="campaigns" handler={Campaigns} />
+    <Route name="reporting" handler={Reporting} />
+    <Route name="inventory" handler={Inventory} />
+  </Route>
+);
+
+Router.run(routes, Router.HistoryLocation, function(Handler) {
+  React.render(<Handler />, document.body);
+});
 
--- src/jsx/navBar.jsx
+++ src/jsx/navBar.jsx
@@ -1,6 +1,23 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+var State = require('react-router').State;
+var Link = require('react-router').Link;
+
+
+var NavLi = React.createClass({
+  mixins: [ State ],
+  
+  render: function() {
+    var isActive = this.isActive(this.props.to, this.props.params, this.props.query);
+    var className = isActive ? 'active' : '';
+    var link = (
+      <Link {...this.props} />
+    );
+
+    return <li className={className}>{link}</li>;
+  }
+});
 
 module.exports= React.createClass({
   render: function() {
@@ -16,15 +33,15 @@ module.exports= React.createClass({
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="#">VOLK Ad Server</a>
+            <Link className="navbar-brand" to="/">VOLK Ad Server</Link>
           </div>
 
           <div className="collapse navbar-collapse" id="nav-collapsable">
             <p className="navbar-text navbar-right">Welcome, <a href="#" className="navbar-link">Nathan</a></p>
             <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Campaigns<span className="sr-only">(current)</span></a></li>
-              <li><a href="#">Reporting</a></li>
-              <li><a href="#">Inventory</a></li>
+              <NavLi to="campaigns">Campaigns</NavLi>
+              <NavLi to="reporting">Reporting</NavLi>
+              <NavLi to="inventory">Inventory</NavLi>
             </ul>
             <form className="navbar-form" role="search">
               <div className="form-group">
