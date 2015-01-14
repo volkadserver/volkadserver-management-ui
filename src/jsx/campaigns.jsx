256da516f4883b8264c39cc47541baecba40532a
Nathan Hagen <nathan@aggronn.com>
2015-01-14 01:28:51 -0500

    Added basic form to create campaign page

--- src/jsx/campaigns.jsx
+++ src/jsx/campaigns.jsx
@@ -66,8 +66,35 @@ module.exports = {
   }),
 
   create: React.createClass({
-   render: function() {
-     return <div className="col-md-12">Create new campaigns here</div>
+    render: function() {
+      return <div className="row">
+          <form className="form-horizontal col-sm-12">
+            <div className="row">
+              <h3 className="col-sm-offset-2 col-sm-6">Add new campaign</h3>
+            </div>
+            <div className="form-group">
+              <label form="newCampaignBasics" className="control-label col-sm-2">Name</label>
+              <div className="col-sm-6">
+                <input type="text" className="form-control" id="newCampaignName" placeholder="New Campaign Name" />
+              </div>
+              <p className="col-sm-4">
+                This is some tip that helps with the stuff. I don't know how long it'll be really. We'll see.
+              </p>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-offset-2 col-sm-6">
+                <div className="btn-toolbar">
+                  <button type="button" className="btn btn-default">
+                    <span className="glyphicon glyphicon-ok"></span> Create
+                  </button>
+                  <button type="button" className="btn btn-info">
+                    <span className="glyphicon glyphicon-plus"></span> Flights
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
    }
   }),
 
