1987cc4c36f5fc8784fd5834ad8da6ee6cd81742
Nathan Hagen <nathan@aggronn.com>
2015-02-08 16:23:17 -0500

    Fix non-bug typo

--- app/actions/orderSourceActionCreators.js
+++ app/actions/orderSourceActionCreators.js
@@ -3,8 +3,8 @@ var orderConstants = require('../constants/orderConstants.js');
 console.log(orderConstants);
 
 var orderSourceActionCreators = marty.createActionCreators({
-  receiveOrders: orderConstants.RECEIVE_ORDERS(function(users) {
-    this.dispatch(users);
+  receiveOrders: orderConstants.RECEIVE_ORDERS(function(orders) {
+    this.dispatch(orders);
   })
 });
 
