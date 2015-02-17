37d640028c7314605a97b24ee15c9952427631a5
Nathan Hagen <nathan@aggronn.com>
2015-02-17 10:38:37 -0500

    Add editOrder page

---
+++ app/components/editOrder.jsx
@@ -0,0 +1,27 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var marty = require('marty');
+var Router = require('react-router');
+
+var orderStore = require('../stores/orderStore');
+
+var orderStateMixin = marty.createStateMixin({
+  listenTo: orderStore,
+  getState: function() {
+    return {
+      order: orderStore.getOrder(this.getParams().id)
+    };
+  }
+});
+
+module.exports = React.createClass({
+  mixins: [orderStateMixin, Router.State],
+
+  render: function() {
+    return <div className="row">
+      Edit an order
+    </div>
+  }
+
+});
--- app/routes.jsx
+++ app/routes.jsx
@@ -5,17 +5,22 @@ var Route = require('react-router').Route;
 var DefaultRoute = require('react-router').DefaultRoute;
 
 var App = require('./components/app.jsx'),
+    reporting = require('./components/reporting.jsx'),
+    inventory = require('./components/inventory.jsx'),
     trafficking = require('./components/trafficking.jsx'),
+
     orderIndex = require('./components/orderIndex.jsx'),
     advertiserIndex = require('./components/advertiserIndex.jsx'),
+
     createOrder = require('./components/createOrder.jsx'),
+    createFlight = require('./components/createFlight.jsx'),
+    createAdvertiser = require('./components/createAdvertiser.jsx'),
+
     order = require('./components/order.jsx'),
     flight = require('./components/flight.jsx'),
     advertiser = require('./components/advertiser.jsx'),
-    createFlight = require('./components/createFlight.jsx'),
-    createAdvertiser = require('./components/createAdvertiser.jsx'),
-    reporting = require('./components/reporting.jsx'),
-    inventory = require('./components/inventory.jsx');
+    
+    editOrder = require('./components/editOrder.jsx');
 
 module.exports = (
   <Route handler={App}>
@@ -33,6 +38,8 @@ module.exports = (
         path="orders/:id/create" handler={createFlight} />
       <Route name="advertiser-index" 
         path="advertisers" handler={advertiserIndex} />
+
+      <Route name="editOrder" path="orders/:id/edit" handler={editOrder} />
     </Route>
     <Route name="reporting" handler={reporting} />
     <Route name="inventory" handler={inventory} />
