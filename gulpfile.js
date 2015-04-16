e8942236f719cfe5c5f9fa118b3c41e00ffa4d86
Nathan Hagen <nathan@aggronn.com>
2015-04-15 22:03:59 -0400

    Add Babel and refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,19 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify'); 
 var concat = require('gulp-concat');
+var babel = require('gulp-babel');
 var haml = require('gulp-haml');
  
 gulp.task('browserify', function() {
-    var bundler = browserify({
-        entries: ['./app/main.jsx'], // Only need initial file, browserify finds the deps
-        transform: [reactify], // We want to convert JSX to normal javascript
-        debug: true, // Gives us sourcemapping
-        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-    });
-    var watcher  = watchify(bundler);
-
-    return watcher
-    .on('update', function () { // When any files update
-        var updateStart = Date.now();
-        console.log('Updating!');
-        watcher.bundle() // Create new bundle that uses the cache for high performance
-        .pipe(source('main.js'))
-    // This is where you add uglifying etc.
-        .pipe(gulp.dest('./build/js/'));
-        console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
-    .bundle() // Create the initial bundle when starting the task
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./build/js/'));
+  var bundler = watchify(browserify('./app/main.jsx', watchify.args));
+  bundler.transform(reactify);
+  bundler.transform(babel);
+  bundler.on('update', function() {
+    return bundler.bundle()
+      .on('error', function(err) { console.log('Error while re-compiling'); })
+      .pipe(source('main.js'))
+      .pipe(gulp.dest('./build/js/'));
+  });
 });
 
 gulp.task('haml', function () {
